2ad88ab0a3f83bb339c9d51b4517221c4ff0d76e
James Bradley <hintonbradley@gmail.com>
2016-10-21 08:19:50 -0700

    updated server

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use('/node_modules', express.static(__dirname + "/node_modules"));
+app.use('/js', express.static(__dirname + "/app/scr"));
+app.use('/style', express.static(__dirname + "/app/style"));
+app.use('/views', express.static(__dirname + "/app/views"));
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
